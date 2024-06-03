9affe3dee1ed1ea8a01429f83f872569b8ecd7cd
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-06-03 23:19:57 +0300

    finaly some code

--- BU.py
+++ BU.py
@@ -1,1 +1,20 @@
-# This is a placeholder
+a = 0
+command = 1
+i = input(">>")[command]
+def ad():
+    global a
+    a += 1
+def m():
+    global a
+    a -= 1
+def q():
+    print(a)
+while True:
+    if "+" in i:
+        ad()
+    if "-" in i:
+        m()
+    if "'" in i:
+        q()
+    if a >= 128:
+        a = 0
