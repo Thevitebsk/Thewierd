364a4776b36689c9bf36299dcce1a4a8bb452d32
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-06-03 23:31:21 +0300

    Update BU.py

--- BU.py
+++ BU.py
@@ -1,6 +1,6 @@
 a = 0
+global command
 command = 1
-i = input(">>")[command]
 def ad():
     global a
     a += 1
@@ -9,12 +9,19 @@ def m():
     a -= 1
 def q():
     print(a)
+def num():
+    global a
+    a=int(input("ENTER YOUR NUMBER HERE:"))
 while True:
+    i = input(">>")
     if "+" in i:
         ad()
     if "-" in i:
         m()
     if "'" in i:
         q()
+    if "№" in i:
+        num()
     if a >= 128:
         a = 0
+    command += 1
