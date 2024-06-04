5a789d6d3c046ca24f2f630707b6ddd156502361
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-06-04 19:50:32 +0300

    Update BU.py

--- BU.py
+++ BU.py
@@ -1,30 +1,53 @@
-at=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-a = 0
+#+ - adds to the 1 cell memory
+#- - subtracts from the 1 cell memory
+#' - prints the 1 cell memoery value
+#` - Gets user input and sets the 1 cell memoery to that value
+#^ - Pushes the value to the one stack memory
+#v - Sets the one memory cell to the value from the one stack memory
+#examples
+#++++++++'^+++++'v++++''+++'^'+++++++++++++++++++++++'v'+++'------'--------' - Hello World
+loat=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ocm = 0
+osm = 0
 e=[]
-def ad():
-    global a
-    a += 1
+def a():
+    global ocm
+    ocm += 1
 def m():
-    global a
-    a -= 1
+    global ocm
+    ocm -= 1
 def q():
-    global a
-    #a = at[5]
-    print(at[a], end='\n')
-def dq():
-    global a
-    a = int(input("CSVAL:"))
+    global ocm
+    print(loat[ocm], end='')
+def iq():
+    global ocm
+    ocm = int(input("CSVAL:"))
+def x():
+    global osm
+    global ocm
+    osm=ocm
+    ocm=0
+def ix():
+    global osm
+    global ocm
+    ocm=osm
+    osm=0
 while True:
     i = input(">>")
     for c in i:
         if c == "+" and "+" not in e:
-            ad()
+            a()
         if c == "-" and "-" not in e:
             m()
         if c == "'" and "'" not in e:
             q()
         if c == "`" and "`" not in e:
-            dq()
+            iq()
+        if c == "^" and "^" not in e:
+            x()
+        if c == "v" and "v" not in e:
+            ix()
+    print("\n")
     if a > 26:
         a = 0
     continue
