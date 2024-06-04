804a79fc755a71a7d4b5b43dbd96702d850f3f2f
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-06-04 18:53:58 +0300

    Update BU.py

--- BU.py
+++ BU.py
@@ -1,3 +1,11 @@
+ASCIITABLE = {
+    ' ':0, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
+    'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12,
+    'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18,
+    'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24,
+    'Y': 25, 'Z': 26
+}
+at=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 a = 0
 e=[]
 def ad():
@@ -7,21 +15,24 @@ def m():
     global a
     a -= 1
 def q():
-    print(a)
-def num():
     global a
-    a=int(input("number:"))
+    a = at[5]
+    print(a)
+def dq():
+    global a
+    a = input
+    
 while True:
     i = input(">>")
-    for char in i:
-        if char == "+" and "+" not in e:
+    for c in i:
+        if c == "+" and "+" not in e:
             ad()
-        if char == "-" and "-" not in e:
+        if c == "-" and "-" not in e:
             m()
-        if char == "'" and "'" not in e:
+        if c == "'" and "'" not in e:
             q()
-        if char == "№" and "№" not in e:
-            num()
-        if a >= 27:
+        if c == "`" and "`" not in e:
+            dq()
+        if a >= "Z":
             a = 0
         continue
