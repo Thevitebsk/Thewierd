5eca1f0da8eedb3c533998bbb588c553fd3841a1
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-20 15:46:32 +0300

    Update main.py

--- bj/main.py
+++ bj/main.py
@@ -1,19 +1,32 @@
+#X++++++++'++++^+++++'v''+++'^'v++++++++'--------'+++'------'--------' - Hello World
 at=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-ocm = 0
+ocm = ""
 osm = ""
 while True:
  osm=""
- ocm=0
+ ocm=""
  for c in input("\n>"):
   if c == "+":
+   if ocm=="":
+    print("X")
+    break
    ocm += 1
   elif c == "-":
+   if ocm=="":
+    print("X")
+    break
    ocm -= 1
   elif c == "'":
+   if osm=="":
+    print("X")
+    break
    print(at[ocm], end='')
   elif c == "`":
    ocm = int(input("CSVAL:"))
   elif c == "^":
+   if ocm=="":
+    print("X")
+    break
    osm=ocm
    ocm=0
   elif c == "v":
@@ -22,8 +35,7 @@ while True:
     break
    ocm=osm
    osm=""
-  elif c == "r":
-   osm=0
+  elif c == "X":
    ocm=0
   else:
    print("X")
