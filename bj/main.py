b0fd0f741796bd5d0068e7d7259910f9cac97cb0
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-20 16:13:55 +0300

    Update main.py

--- bj/main.py
+++ bj/main.py
@@ -1,47 +1,26 @@
 at=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-ocm = ""
-ax=0
+ocm=0
+osm=""
 while True:
  osm=""
- ocm=""
+ ocm=0
  for c in input("\n>"):
   if c == "+":
-   if ocm=="":
-    print("X")
-    break
    ocm += 1
   elif c == "-":
-   if ocm=="":
-    print("X")
-    break
    ocm -= 1
   elif c == "'":
-   if osm=="":
-    print("X")
-    break
    print(at[ocm], end='')
   elif c == "`":
    ocm = int(input("CSVAL:"))
   elif c == "^":
-   if ocm=="":
-    print("X")
-    break
    osm=ocm
    ocm=0
   elif c == "v":
-   if osm=="":
-    print("X")
-    break
    ocm=osm
    osm=""
-  elif c == "X":
-   ax+=1
-   ocm=0
   else:
    print("X")
    break
   if ocm < 0 or ocm > 25:
    ocm = 0
-  if ax > 1:
-   print("X")
-   break
