fe5fcfe4c53fe476b3b34b071f9ba0089332c76d
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-20 15:23:06 +0300

    Update Branjunk.py

--- bj/Branjunk.py
+++ bj/Branjunk.py
@@ -2,29 +2,29 @@ at="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ocm = 0
 osm = ""
 while True:
-    for c in input("\n>"):
-        if c == "+":
-            ocm += 1
-        elif c == "-":
-            ocm -= 1
-        elif c == "'":
-            print(at[ocm], end='')
-        elif c == "`":
-            ocm = int(input("CSVAL:"))
-        elif c == "^":
-            osm=ocm
-            ocm=0
-        elif c == "v":
-            if osm=="":
-                print("X")
-                break
-            ocm=osm
-            osm=""
-        elif c == "r":
-            osm=0
-            ocm=0
-        elif ocm < 0 or ocm > 25:
-            ocm = 0
-        else:
-            print("X")
-            break
+ for c in input("\n>"):
+  if c == "+":
+   ocm += 1
+  elif c == "-":
+   ocm -= 1
+  elif c == "'":
+   print(at[ocm], end='')
+  elif c == "`":
+   ocm = int(input("CSVAL:"))
+  elif c == "^":
+   osm=ocm
+   ocm=0
+  elif c == "v":
+   if osm=="":
+    print("X")
+    break
+   ocm=osm
+   osm=""
+  elif c == "r":
+   osm=0
+   ocm=0
+  elif ocm < 0 or ocm > 25:
+   ocm = 0
+  else:
+   print("X")
+   break
