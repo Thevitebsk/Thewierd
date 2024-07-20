baafbeeefe666cd37d5c7a32fd1b907c3643388a
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-20 15:20:09 +0300

    Update Branjunk.py

--- bj/Branjunk.py
+++ bj/Branjunk.py
@@ -1,60 +1,30 @@
-#+ - adds to the 1 cell memory
-#- - subtracts from the 1 cell memory
-#' - prints the 1 cell memory value as the "at" charecter
-#` - Gets user input and sets the 1 cell memoery to that value
-#^ - Pushes the value to the one stack memory
-#v - Sets the one memory cell to the value from the one stack memory
-#examples
-#+++++++'^++++'v++++''+++'++++++++'--------'+++'^+++++++++++'--------' - Hello World
 at="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ocm = 0
-osm = 0
-e=[]
-def a():
-    global ocm
-    ocm += 1
-def m():
-    global ocm
-    ocm -= 1
-def q():
-    global ocm
-    print(at[ocm], end='')
-def iq():
-    global ocm
-    ocm = int(input("CSVAL:"))
-def x():
-    global osm
-    global ocm
-    osm=ocm
-    ocm=0
-def ix():
-    global osm
-    global ocm
-    ocm=osm
-    osm=0
-def r():
-        global osm
-        global ocm
-        osm=0
-        ocm=0
+osm = ""
 while True:
-    i = input(">")
-    for c in i:
-        if c == "+" and "+" not in e:
-            a()
-        if c == "-" and "-" not in e:
-            m()
-        if c == "'" and "'" not in e:
-            q()
-        if c == "`" and "`" not in e:
-            iq()
-        if c == "^" and "^" not in e:
-            x()
-        if c == "v" and "v" not in e:
-            ix()
-        if c == "r" and "r" not in e:
-            r()
-        if ocm < 0 or ocm > 25:
+    for c in input("\n>"):
+        if c == "+":
+            ocm += 1
+        elif c == "-":
+            ocm -= 1
+        elif c == "'":
+            print(at[ocm], end='')
+        elif c == "`":
+            ocm = int(input("CSVAL:"))
+        elif c == "^":
+            osm=ocm
+            ocm=0
+        elif c == "v":
+            if osm=="":
+                print("X")
+                break
+            ocm=osm
+            osm=""
+        elif c == "r":
+            osm=0
+            ocm=0
+        elif ocm < 0 or ocm > 25:
             ocm = 0
-    print("\n")
-    continue
+        else:
+            print("X")
+            break
