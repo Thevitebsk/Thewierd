9c6b0fe678b3a92c314b6b0ef35e0ef70e731275
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-09-22 19:54:00 +0300

    Update main.py

--- bj/main.py
+++ bj/main.py
@@ -1,12 +1,12 @@
-a=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";ocm=0;osm=""
+a=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";c=0;s=""
 while True:
  s="";c=0
  for c in input("\n>"):
-  if c == "+":c+=1
-  elif c == "-":c-=1
-  elif c == "'":print(a[c], end='')
-  elif c == "`":c=int(input("CSVAL:"))
-  elif c == "^":s=c;c=0
-  elif c == "v":c=s;s=""
+  if c=="+":c+=1
+  elif c=="-":c-=1
+  elif c=="'":print(a[c], end='')
+  elif c=="`":c=int(input("CSVAL:"))
+  elif c=="^":s=c;c=0
+  elif c=="v":c=s;s=""
   else:print("X");break
   if c<0 or c>25:c=0
