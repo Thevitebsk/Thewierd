26cb599913493f85cb3684b532008e7efa256ee1
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-23 12:52:57 +0300

    Update main.py

--- bj/main.py
+++ bj/main.py
@@ -1,13 +1,13 @@
-a=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";ac=0;m=0
+a=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";b=0;m=0
 while True:
- s=0;ac=0
+ m=0;b=0
  for c in input("\n>"):
-  if c=="+":ac+=1
-  elif c=="-":ac-=1
-  elif c=="'":print(end=a[ac])
-  elif c=="`":ac=int(input("num:"))
-  elif c=="^":m=ac;ac=0
-  elif c=="v":ac=m;m=0
-  elif c=="#":ac=0
-  if ac<0:ac=25
-  elif ac>25:ac=0
+  if c=="+":b+=1
+  elif c=="-":b-=1
+  elif c=="'":print(end=a[b])
+  elif c=="`":b=int(input("num:"))
+  elif c=="^":m=b;b=0
+  elif c=="v":b=m;m=0
+  elif c=="#":b=0
+  if b<0:b=25
+  elif b>25:b=0
