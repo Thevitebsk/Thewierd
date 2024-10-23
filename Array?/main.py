0df0eca0d9fe7e3b15ace742aa0c903749fec25f
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-23 12:42:19 +0300

    Create main.py

---
+++ Array?/main.py
@@ -0,0 +1,20 @@
+print("Array?")
+c="""[Hello\, world!]""";p=0;m=0;s=[];ar=[]
+while p<len(c):
+ if c[p]=="[" and m==0:m=1
+ elif m==1:
+  if c[p]=="," and c[p-1]!="\\":
+   while len(s)>1:a=str(s[0])+str(s[1]);s.pop(0);s.pop(0);s.reverse();s.append(a);s.reverse()
+   ar.append(s[0]);s.pop(0)
+  s.append(c[p])
+  if c[p]=="]":
+   m=0;s.pop()
+   if s[0]==",":
+    s.pop(0)
+   while len(s)>1:a=str(s[0])+str(s[1]);s.pop(0);s.pop(0);s.reverse();s.append(a);s.reverse()
+   ar.append(s[0].replace("\\",""));s.pop(0)
+ elif c[p]=="∃":
+  while c[p]!="∄":
+   p+=1
+ p+=1
+while len(ar)>0:print(ar[0]);ar.pop(0)
