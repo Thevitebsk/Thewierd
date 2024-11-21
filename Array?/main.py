e98725a4e7118b20d55d35c815f5a9e819ec4d8b
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-21 23:06:46 +0300

    tried to minfy it

--- Array?/main.py
+++ Array?/main.py
@@ -1,20 +1,21 @@
 print("Array?")
-c="""□""";p=0;m=0;s=[];ar=[];i=input();i=i.split()
+c="""[Hello\, world!]""";p=0;m=0;s=[];ar=[];i=''
 while p<len(c):
  if c[p]=="[" and m==0:m=1
  elif m==1:
   if c[p]=="," and c[p-1]!="\\":
-   while len(s)>1:a=str(s[0])+str(s[1]);s.pop(0);s.pop(0);s.reverse();s.append(a);s.reverse()
-   ar.append(s[0]);s.pop(0)
+   s.reverse()
+   while len(s)>1:s.append(s.pop()+s.pop())
+   ar.append(s[0]);s.pop()
   s.append(c[p])
   if c[p]=="]":
    m=0;s.pop()
-   if s[0]==",":
-    s.pop(0)
-   while len(s)>1:a=str(s[0])+str(s[1]);s.pop(0);s.pop(0);s.reverse();s.append(a);s.reverse()
+   if s[0]==",":s.pop(0)
+   s.reverse()
+   while len(s)>1:s.append(s.pop()+s.pop())
    ar.append(s[0].replace("\\",""));s.pop(0)
  elif c[p]=="∃":
   while c[p]!="∄":p+=1
- elif c[p]=="□":ar.append(i.pop(0))
+ elif c[p]=="□":ar.append(input())
  p+=1
-while len(ar)>0:print(ar[0]);ar.pop(0)
+print("\n".join(ar))
