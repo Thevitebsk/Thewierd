a1ded023b0c4c5821eb48b63698c6fabd1035094
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-23 11:38:10 +0300

    Update mini.py

--- ljapl/mini.py
+++ ljapl/mini.py
@@ -1,1 +1,12 @@
-
+print("""
+L       J    A    PPPP L
+L       J   A A   P  P L
+L       J  AAAAA  PPPP L
+LLLL JJJ  A     A P    LLLL""")
+n="\n"
+while True:
+  ce=f"0123456789{n} abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]`|~"
+  s=[];nc=0;b=[];e=0;num=[]
+  out=[];inp=input("code:")
+  for _ in range(10):num.append(str(_))
+  if inp[nc]=="+":s.append(int(s.pop(0))+int(s.pop(0)))
