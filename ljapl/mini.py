c4e027ba58cf772da538742d470d5622042db76d
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-23 12:16:52 +0300

    Update mini.py

--- ljapl/mini.py
+++ ljapl/mini.py
@@ -10,4 +10,12 @@ while True:
   s=[];nc=0;b=[];e=0;num=[]
   out=[];inp=input("code:")
   for _ in range(10):num.append(str(_))
-  if inp[nc]=="+":s.append(int(s.pop(0))+int(s.pop(0)))
+  if inp=="exit":break
+  while len(inp)>nc:
+    if inp[nc]=="+":s.append(int(s.pop(0))+int(s.pop(0)))
+    elif inp[nc]in num[0:9]:s.append(int(inp[nc]))
+    elif inp[nc]=="a":s.append(10)
+    elif inp[nc]=="|":out=str(out);print("".join(out[0:len(out)]))
+    elif inp[nc]=="n":out.append(int(s.pop(0)))
+    else:print("found an unknown command at",nc+1);break
+    nc+=1
