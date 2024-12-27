9abea2f21d630eadb56a548c33febac3d8522892
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-27 22:53:27 +0300

    Update main.py

--- *t*/main.py
+++ *t*/main.py
@@ -1,4 +1,4 @@
-num=list(map(str,range(10)));stack=[]
+stack=[];var={}
 out="";code="""
 
 """
@@ -6,8 +6,14 @@ s=0;tr=0;p=0;code=code[1:len(code)-1]
 if code=="":s=1;out+=input()
 while len(code)>p and s==0:
  if len(out)>1023:tr=1;break
- if code[p]in num:stack.append(int(num[int(code[p])]))
+ if code[p]in list(map(str,range(10))):stack.append(int(code[p]))
+ if code[p]=="•":var[code[p+1]]=stack.pop()
+ if code[p]==",":
+  try:
+   if code[p+1] in var:stack.append(var[code[p+1]])
+   else:out+=str(stack.pop())
+  except:out+=str(stack.pop())
  p+=1
-if tr:print("T")
 print("~"*15+" RESULT "+"~"*15)
-print(out,"~"*38,"STACK: "+str(stack),sep="\n")
+print(out,"~"*38,"STACK: "+str(stack),"VARIABLES: "+str(var),sep="\n")
+if tr:print("TRUNCATED")
