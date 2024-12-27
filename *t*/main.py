e28b121a4b9e51f27d363c0c1c7af3917b72eff3
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-27 22:37:01 +0300

    Update main.py

--- *t*/main.py
+++ *t*/main.py
@@ -2,9 +2,12 @@ num=list(map(str,range(10)));stack=[]
 out="";code="""
 
 """
-s=0;p=0;code=code[1:len(code)-1]
+s=0;tr=0;p=0;code=code[1:len(code)-1]
 if code=="":s=1;out+=input()
 while len(code)>p and s==0:
+ if len(out)>1023:tr=1;break
  if code[p]in num:stack.append(int(num[int(code[p])]))
  p+=1
-print("~"*15+" RESULT "+"~"*15,out,"~"*38,"STACK: "+str(stack),sep="\n")
+if tr:print("T")
+print("~"*15+" RESULT "+"~"*15)
+print(out,"~"*38,"STACK: "+str(stack),sep="\n")
