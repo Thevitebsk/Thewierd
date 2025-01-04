0b781eed11bb2fb1caa5aad27a88efbd7e49e65b
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-04 14:54:45 +0300

    Update main.py

--- *t*/main.py
+++ *t*/main.py
@@ -1,19 +1,22 @@
 stack=[];var={}
 out="";code="""
-
+~i
 """
-s=0;tr=0;p=0;code=code[1:-1]
+s=0;tr=0;p=0;code=code[1:len(code)-1]
 if code=="":s=1;out+=input()
 while len(code)>p and s==0:
- if len(out)>1023:tr=1;break
  if code[p]in list(map(str,range(10))):stack.append(int(code[p]))
- if code[p]=="•":var[code[p+1]]=stack.pop()
- if code[p]==",":
+ elif code[p]=="•":var[code[p+1]]=stack.pop();p+=1
+ elif code[p]==",":
   try:
-   if code[p+1] in var:stack.append(var[code[p+1]])
+   if code[p+1]in var:stack.append(var[code[p+1]])
    else:out+=str(stack.pop())
   except:out+=str(stack.pop())
+ elif code[p]=="~":
+  try:var[code[p+1]]=input()
+  except IndexError:stack.append(input())
+ if len(out)>1023:tr=1;break
  p+=1
 print("~"*15+" RESULT "+"~"*15)
 print(out,"~"*38,"STACK: "+str(stack),"VARIABLES: "+str(var),sep="\n")
-if tr:print("TRUNCATED")
+if tr==1:print("TRUNCATED")
