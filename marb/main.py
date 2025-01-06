efcccd0d16755e925699bd4c3c95e9c00d9b2daa
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-06 23:41:16 +0300

    Update main.py

--- marb/main.py
+++ marb/main.py
@@ -1,7 +1,7 @@
 import sys,time
-y=x=mv=0;st=[];arg=sys.argv[1:];debug=1;s=1
+y=x=mv=0;st=[];arg=sys.argv[1:];debug=1;s=1;out=""
 while arg:
- if arg[0]=="-d":debug=1
+ if arg[0]=="-d":debug=0
 c="""
 _
 """
@@ -10,10 +10,16 @@ while 1:
  if debug==1:print(f"STEP {s}",f"{a[y][x]} {y} {x} {mv}","==================="*2,sep="\n");s+=1;time.sleep(0.5)
  if a[y][x]=="\\":x+=1;y-=1
  elif a[y][x]=="/":x-=1;y-=1
- elif a[y][x]=="_":print("".join(s));break
+ elif a[y][x]=="_":print(out);break
  elif a[y][x]=="=":y=0-1
  elif a[y][x]=="[":st.append(mv)
  elif a[y][x]=="+":mv+=1
  elif a[y][x]=="-":mv-=1
+ elif a[y][x]=="^":
+  if st.pop():x+=1;y-=1
+  else:x-=1;y-=1
+ elif a[y][x]=="?":st.append(input())
+ elif a[y][x]==".":out+=str(st.pop())
+ if len(out)=="32":print(out);break
  y+=1
  if y>len(a)-1:print("Your marble has escaped");break
