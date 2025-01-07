b5cd086ab35ea0afaf8a9379b93a3422e957d72e
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-07 11:31:27 +0300

    Update main.py

--- marb/main.py
+++ marb/main.py
@@ -8,21 +8,19 @@ _
 """
 a=c[1:-1].split("\n")
 while 1:
- try:
-  if debug==1:print(f"STEP {s}",f"{a[y][x]} {y} {x} {mv}","==================="*2,sep="\n")
-  if a[y][x]=="\\":x+=1;y-=1
-  elif a[y][x]=="/":x-=1;y-=1
-  elif a[y][x]=="_":print(out);break
-  elif a[y][x]=="=":y=0-1
-  elif a[y][x]=="[":st.append(mv)
-  elif a[y][x]=="+":mv+=1
-  elif a[y][x]=="-":mv-=1
-  elif a[y][x]=="^":
-   if st.pop():x+=1;y-=1
-   else:x-=1;y-=1
-  elif a[y][x]=="?":st.append(int(input()))
-  elif a[y][x]==".":out+=str(st.pop())
-  if s==4096//dd:print(out);break
- except:a[y][x]=" "
+ if debug==1:print(f"STEP {s}",f"{a[y][x]} {y} {x} {mv}","==================="*2,sep="\n")
+ if a[y][x]=="\\":x+=1;y-=1
+ elif a[y][x]=="/":x-=1;y-=1
+ elif a[y][x]=="_":print(out);break
+ elif a[y][x]=="=":y=0-1
+ elif a[y][x]=="[":st.append(mv)
+ elif a[y][x]=="+":mv+=1
+ elif a[y][x]=="-":mv-=1
+ elif a[y][x]=="^":
+  if st.pop():x+=1;y-=1
+  else:x-=1;y-=1
+ elif a[y][x]=="?":st.append(int(input()))
+ elif a[y][x]==".":out+=str(st.pop())
+ if s==4096//dd:print(out);break
  y+=1;s+=1
  if y>len(a)-1:print("Your marble has escaped");break
