f772615bb7083072c6abdc5ed1172f223b1dd51b
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-20 01:02:39 +0300

    Update main.py

--- UNAI/main.py
+++ UNAI/main.py
@@ -5,7 +5,7 @@ def push_int():
     while 1:
      if point+1>len(code):break
      tempstack.append(code[point]);point+=1
-     if code[point]==" ":break
+     if code[point]not in list(map(str,range(10))):point-=1;break
     stack.append("".join(tempstack));tempstack.clear()
 while len(code)>point:
     if code[point]in list(map(str,range(10))):push_int()
@@ -14,5 +14,6 @@ while len(code)>point:
         point+=1
         while code[point]!="\"":tempstack.append(code[point]);point+=1
         stack.append("".join(tempstack));tempstack.clear()
+    elif code[point]=="#":stack.pop()
     point+=1
-if force==0:print("\n".join(stack))
+if force==0:print(" ".join(stack))
