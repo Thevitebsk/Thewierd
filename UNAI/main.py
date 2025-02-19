e7a4fbd3aa48a9f6676fd16079f31cab0292e357
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-20 00:51:24 +0300

    Update and rename main.py to main.py

---
+++ UNAI/main.py
@@ -0,0 +1,18 @@
+# --UNAI v1.0-- #
+stack=[];tempstack=[];point=force=0;code=input()
+def push_int():
+    global point
+    while 1:
+     if point+1>len(code):break
+     tempstack.append(code[point]);point+=1
+     if code[point]==" ":break
+    stack.append("".join(tempstack));tempstack.clear()
+while len(code)>point:
+    if code[point]in list(map(str,range(10))):push_int()
+    elif code[point]=="!":print(stack.pop());force=1
+    elif code[point]=="\"":
+        point+=1
+        while code[point]!="\"":tempstack.append(code[point]);point+=1
+        stack.append("".join(tempstack));tempstack.clear()
+    point+=1
+if force==0:print("\n".join(stack))
