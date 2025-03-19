38b0b0123b469233ebe830baca72a9f4d2445a1d
Gaham <glebsemenov1mail.ru@gmail.com>
2025-03-19 22:32:16 +0300

    Update main.py

--- UNAI/main.py
+++ UNAI/main.py
@@ -1,4 +1,4 @@
-# --UNAI v1.0-- #
+# --UNAI-- #
 s=[];ts=[];p=io=0;c=input()
 def push_int():
     global p
@@ -6,9 +6,10 @@ def push_int():
      if p+1>len(c):break
      ts.append(c[p]);p+=1
      if c[p]not in list(map(str,range(10))):p-=1;break
+    while ts[0]=="0":ts.pop(0)
     s.append("".join(ts));ts.clear()
 while len(c)>p:
-    if c[p]in list(map(str,range(10))):push_int()
+    if c[p]in"0123456789":push_int()
     elif c[p]==".":print(s.pop());io=1
     elif c[p]=="\"":
         p+=1
